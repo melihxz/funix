4d85eaa401c682d438c3d97318e276755ede058a
Xander Zheleva <lastsatanix@gmail.com>
2024-08-19 10:13:05 +0300

    Create shell.h

---
+++ shell/shell.h
@@ -0,0 +1,13 @@
+// shell/shell.h
+
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdint.h>
+
+// Kabuk başlatma ve kapatma işlemleri
+void shell_init();
+void shell_run();
+void shell_shutdown();
+
+#endif // SHELL_H
