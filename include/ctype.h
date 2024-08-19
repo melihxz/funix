c71768934fea8dd55a00bafe5ecbcb35a5e51974
Xander Zheleva <lastsatanix@gmail.com>
2024-08-19 10:01:23 +0300

    Create ctype.h

---
+++ include/ctype.h
@@ -0,0 +1,14 @@
+// include/ctype.h
+
+#ifndef CTYPE_H
+#define CTYPE_H
+
+int isdigit(int c);
+int isalpha(int c);
+int isalnum(int c);
+int islower(int c);
+int isupper(int c);
+int tolower(int c);
+int toupper(int c);
+
+#endif // CTYPE_H
