0cf07d933a160405dee9fb1038ba7f2954aca2e6
Xander Zheleva <lastsatanix@gmail.com>
2024-08-19 10:06:33 +0300

    Create math.h

---
+++ libc/math.h
@@ -0,0 +1,19 @@
+// libc/math.h
+
+#ifndef MATH_H
+#define MATH_H
+
+#define M_PI 3.14159265358979323846
+
+// Temel matematiksel işlevler
+double sqrt(double x);
+double pow(double base, double exponent);
+double sin(double x);
+double cos(double x);
+double tan(double x);
+double asin(double x);
+double acos(double x);
+double atan(double x);
+double atan2(double y, double x);
+
+#endif // MATH_H
