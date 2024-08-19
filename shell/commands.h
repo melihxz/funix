fd10845ad46a9affffd0e66e778c36ba46cff682
Xander Zheleva <lastsatanix@gmail.com>
2024-08-19 10:12:18 +0300

    Create commands.h

---
+++ shell/commands.h
@@ -0,0 +1,11 @@
+// shell/commands.h
+
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+// Yerleşik komutların işlevleri
+int cmd_help(int argc, char **argv);
+int cmd_exit(int argc, char **argv);
+// Buraya daha fazla komut ekleyebilirsiniz
+
+#endif // COMMANDS_H
